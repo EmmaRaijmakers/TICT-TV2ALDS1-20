afca39aa0a89aed5dee8ada52912ee8d2495e714
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2023-01-26 15:51:53 +0100

    Added first version of rollout function

--- Gomoku Eindopdracht/my_agent.py
+++ Gomoku Eindopdracht/my_agent.py
@@ -3,6 +3,7 @@ import gomoku
 import time
 import numpy as np
 from gomoku import Board, Move, GameState
+import GmUtils
 
 
 class my_player:
@@ -11,6 +12,8 @@ class my_player:
     2) it specifies the required methods that will be used by the competition to run
     your player
     """
+    #TODO tree en child vars? etc
+    tree = {}
 
     def __init__(self, black_: bool = True):
         """Constructor for the player."""
@@ -26,9 +29,67 @@ class my_player:
     def find_spot_to_expand(self, moves):
         return random.choice(moves)
 
-    def rollout(self, leaf) -> float:
-        #TODO implement this function
-        return 0.0
+    def rollout(self, leaf, moves) -> float:
+        #TODO Test this function
+
+        #TODO voeg dit toe \/
+        """NumPy copy:
+
+        >>> pybench "cp_np = np.copy(ref_np)"
+        100000 loops, best of 3: 6.03 usec per loop
+        Copying to pre-created NumPy array:
+
+        >>> pybench "np.copyto(cp_np, ref_np)"
+        100000 loops, best of 3: 4.52 usec per loop"""
+
+        current_moves = moves.copy()
+        current_board = Board.deepcopy()
+
+        #TODO wat do als er geen valid moves meer zijn?
+        # if len(current_moves) == 0:
+        #     return 0.0
+
+        end_state = False
+
+        #current player to make a move
+        current_player_mine = True
+
+        #to make sure the first move is the par
+        first_run = True
+
+        while not end_state:
+            if first_run:
+                action = leaf
+                first_run = False
+            else:
+                action = random.choice(current_moves)
+            current_moves.remove(action)
+
+            #play a move based on who is to move and what color they have
+            if current_player_mine and self.black:
+                GmUtils.addMoveToBoard(current_board, action, 1) 
+            elif current_player_mine and not self.black:
+                GmUtils.addMoveToBoard(current_board, action, 2) 
+            elif not current_player_mine and self.black:
+                GmUtils.addMoveToBoard(current_board, action, 2)
+            elif not current_player_mine and not self.black:
+                GmUtils.addMoveToBoard(current_board, action, 1)  
+
+            #check if the game is won
+            win = GmUtils.isWinningMove(action, current_board)
+
+            #if win return the right value based on which player won (or neither)
+            if win and current_player_mine:
+                return 1
+            elif win and not current_player_mine:
+                return 0
+            elif len(current_moves) == 0:
+                return 0.5
+
+            end_state = win
+        
+        #niet nodig ??
+        return 0.5
 
     def backup_value(self, leaf, val):
         #TODO implement this function
@@ -58,9 +119,11 @@ class my_player:
 
         while cur_time < max_time:
             # n_leaf = random.choice(moves)
+
             n_leaf = self.find_spot_to_expand(moves)
 
-            val = self.rollout(n_leaf)
+            # val = self.rollout(n_leaf)
+            val = self.rollout(n_leaf, moves)
             self.backup_value(n_leaf, val)
 
             if val > cur_best_value:
